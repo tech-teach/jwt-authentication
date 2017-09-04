613e71964e474a937d2a941882717575448e8fe1
Oscar David Arbeláez <oscar.arbelaez@adroll.com>
2017-09-04 18:18:27 -0500

    Use antisymmetric signing and verification

--- service/app.py
+++ service/app.py
@@ -7,7 +7,18 @@ import jwt
 from apistar import http, Route, Component
 from apistar.frameworks.wsgi import WSGIApp as App
 
-KEY = 'secret'
+
+# I'm not thaaat thrustworthy, or don't want to compromise security of other services
+# therefore I hold a public key
+PUBLIC_KEY = '''
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCnppZHSpyoV4KYr+XGBh+\
+tNJfHv9ZOkGpKnXxNstyOvVBXiTLGzAb5DMOXsQhOiRJ3fK8jp4xu/o3GRGq\
+6YbsznSY0qboOWNEyHbr9HjPXuvggWA1mvrQVDjxwdYTvGXk7gg1LKMcYgrq\
+8d6kOxsd4Yk1gBt41MHQX0IeXpMph5k0CdhXKQ90Lf9anlPMs0/oqAO8YyXIs\
+eHVnIXRdq75u6ka+8x+cv2HDTj9jt4KvySdUU5V8Ll6U23o0zEq2O62b8So\
+tbj21lG9Ubki82EWavYbhDOZFPpaZpaCrX0v15nhLiqgOxh4TtITctaoE\
+9H87cvwYLD3AkR4b4DvmT9Cr jwt-exampe
+'''.strip()
 
 
 class User(object):
@@ -32,7 +43,7 @@ def authenticate_user(authorization: http.Header):
     scheme, token = authorization.split()
     if scheme.lower() != 'bearer':
         return None
-    return User(jwt.decode(token, KEY, algorithms=['HS256']))
+    return User(jwt.decode(token, PUBLIC_KEY, algorithms=['RS256']))
 
 
 def protected(user: User):
